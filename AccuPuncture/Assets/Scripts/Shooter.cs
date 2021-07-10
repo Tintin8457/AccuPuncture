e043b455b546017abe561cb231ac67869dba1f8b
bloo7883 <34141279+bloo7883@users.noreply.github.com>
2021-07-09 23:49:46 -0400

    LookVertically

--- AccuPuncture/Assets/Scripts/Shooter.cs
+++ AccuPuncture/Assets/Scripts/Shooter.cs
@@ -6,7 +6,10 @@ public class Shooter : MonoBehaviour
 {
     public GameObject projectile;
     public int shootLimit = 1;
-    
+
+    private float verticalLook;
+    public int lookSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     { 
@@ -16,13 +19,18 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Look up and down
+        verticalLook = Input.GetAxis("Mouse X") * lookSpeed;
+        transform.Rotate(verticalLook, 0, 0);
+
+        //Shoot bullets
         if (Input.GetKey(KeyCode.Mouse0))
         {
             if (shootLimit == 1)
             {
                 //Input screenray = Camera.main.ScreenPointToRay();
                 Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
-                shootLimit -= 1;
+                shootLimit -= 1; //Lower shoot limit
             }             
         }
     }
