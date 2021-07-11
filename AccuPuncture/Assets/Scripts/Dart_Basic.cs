decb2b9fffab2ca9bbfb1a837f9319327b200e47
Steven Fisher <stevushka.f@gmail.com>
2021-07-11 11:56:44 -0400

    Player and Patient Prefabs

---
+++ AccuPuncture/Assets/Scripts/Dart_Basic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart_Basic : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag == "Patient")
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            
+        }
+    }
+}
